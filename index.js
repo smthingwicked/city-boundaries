c976298010d8c93242fef0fecd911280ea7c0636
smthingwicked <spartak.71.1994@rambler.ru>
2021-01-07 18:59:26 +0300

    change algorithms in functions

--- index.js
+++ index.js
@@ -2,58 +2,60 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 const delay = (ms) => new Promise((result) => setTimeout(() => result(), ms));
-const file = 'cities.txt';
 
-//make Array of cties from txt file
-const cities = fs
-  .readFileSync(file, 'utf-8', (data) => data)
-  .trim()
-  .split('\n');
-
-// Take city OSM ID on nominatim.openstreetmap
+// Take city OSM ID from nominatim.openstreetmap
 const findCityOSMid = async (city) => {
-  const cityUrl = encodeURI(
-    `https://nominatim.openstreetmap.org/search?q=${city}&format=json`
-  );
+  const cityUrl = encodeURI(`https://nominatim.openstreetmap.org/search?q=${city}&format=json`);
+
   const response = await fetch(cityUrl);
   const data = await response.json();
-  for (let i in data) {
-    if (data[i].display_name.match(/Россия/) !== null) {
-      return data[i].osm_id;
-    }
-  }
-  throw Error(`Не найден идентификатор для города ${city}`);
+
+  const cityOSMid = data.find(({ class: klazz, type, display_name }) => {
+    return klazz === 'place' && ['town', 'city'].includes(type) && display_name.includes('Россия');
+  });
+
+  return cityOSMid ? cityOSMid.osm_id : null;
 };
 
-//Take border data and save in file
+//Take city border
 const takeBorder = async (city) => {
   await delay(2000);
   const cityOSMid = await findCityOSMid(city);
+
   const borderUrl = encodeURI(
     `https://polygons.openstreetmap.fr/get_geojson.py?id=${cityOSMid}&params=0`
   );
 
   const response = await fetch(borderUrl);
 
-  const data = await response.json();
+  if (!response.headers.get('content-encoding')) {
+    console.error(`Не найдены геоданные для города ${city}, OSM OD = ${cityOSMid}`);
+    return null;
+  }
 
-  await fs.writeFile(`./geojsons/${city}.txt`, JSON.stringify(data), (err) => {
+  return await response.json();
+};
+
+const makeGeoJson = async (filePath) => {
+  let boundaries = {};
+
+  //make Array of cties from txt file
+  const cities = await fs
+    .readFileSync(filePath, 'utf-8', (data) => data)
+    .trim()
+    .split('\n');
+
+  for (let i in cities) {
+    const city = cities[i];
+
+    const boundary = await takeBorder(city);
+
+    boundaries = boundary ? { ...boundaries, [city]: boundary } : boundaries;
+  }
+
+  await fs.writeFile(`./geojsons/boundaries.json`, JSON.stringify(boundaries), (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
 };
-
-const makeGeoJson = async () => {
-  for (let i in cities) {
-    const city = cities[i];
-    try {
-      await takeBorder(city);
-    } catch (e) {
-      console.error(
-        `При отправке запроса возникла ошибка!\n ${e} \n Переходим к следующему городу`
-      );
-      continue;
-    }
-  }
-};
-makeGeoJson();
+makeGeoJson('cities.txt');
